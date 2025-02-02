9a4ca9d33584fa9b9f0de126cb3c7236dd03e0dd
Johannes Bosecker <j.bosecker.dev@icloud.com>
2025-02-02 15:04:07 +0100

    Fixed deprecation warning.

--- eltako/__init__.py
+++ eltako/__init__.py
@@ -64,11 +64,7 @@ async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> b
     await usb_gateway.async_setup()
     eltako_data[ELTAKO_GATEWAY] = usb_gateway
     
-    for platform in PLATFORMS:
-        hass.async_create_task(
-            hass.config_entries.async_forward_entry_setup(config_entry, platform)
-        )
-        
+    await hass.config_entries.async_forward_entry_setups(config_entry, PLATFORMS)
     return True
 
 
